e94800b13f44b4a9dffb7b01225f764c02e495c3
aliarnoos <alimarnoos@gmail.com>
2025-03-24 22:00:49 +0300

    add: infinite socket loop

--- src/main.zig
+++ src/main.zig
@@ -9,21 +9,24 @@ pub fn main() !void {
     const socket = try SocketConfig.Socket.init();
     try stdout.print("Server Address: {any}\n", .{socket._address});
     var server = try socket._address.listen(.{});
-    const connection = try server.accept();
-    var buffer: [1000]u8 = undefined;
-    for (0..buffer.len) |i| {
-        buffer[i] = 0;
-    }
-    _ = try Request.read_request(connection, buffer[0..buffer.len]);
 
-    const request = Request.parse_request(buffer[0..buffer.len]);
+    while (true) {
+        const connection = try server.accept();
+        var buffer: [1000]u8 = undefined;
+        for (0..buffer.len) |i| {
+            buffer[i] = 0;
+        }
+        _ = try Request.read_request(connection, buffer[0..buffer.len]);
 
-    try stdout.print("{any}\n", .{request});
-    if (request.method == Method.GET) {
-        if (std.mem.eql(u8, request.uri, "/")) {
-            try Response.send_200(connection);
-        } else {
-            try Response.send_404(connection);
+        const request = Request.parse_request(buffer[0..buffer.len]);
+
+        try stdout.print("{any}\n", .{request});
+        if (request.method == Method.GET) {
+            if (std.mem.eql(u8, request.uri, "/")) {
+                try Response.send_200(connection);
+            } else {
+                try Response.send_404(connection);
+            }
         }
     }
 }
